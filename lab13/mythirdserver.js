4bc71647191701ae6bafdb4e50f14b20046b3ab8
ConnorM1998 <1612706@rgu.ac.uk>
2018-04-17 21:27:37 +0100

    wokr

--- lab13/mythirdserver.js
+++ lab13/mythirdserver.js
@@ -3,6 +3,6 @@ var knockknock = require('knock-knock-jokes');
 
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time is currently: " + currentdate.myDateTime());
+    var randomJoke = knockknock()
     res.end(randomJoke);
 }).listen(8080);
