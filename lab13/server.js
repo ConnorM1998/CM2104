9c4e2fdbbbf13b01c8f6e58773474ce417da89eb
ConnorM1998 <1612706@rgu.ac.uk>
2018-04-17 21:45:32 +0100

    work

--- lab13/server.js
+++ lab13/server.js
@@ -9,4 +9,10 @@ app.get('/test', function(req, res){
   res.send("This is route 2")
 });
 
+app.get('/add', function(req, res){
+  var x = req.query.x;
+  var y = req.query.y;
+  res.send("X + Y = " + (x+y));
+});
+
 app.listen(8080);
