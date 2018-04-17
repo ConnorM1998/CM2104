e81aeb2601f3d769f49644890b4253b8529a2ff8
ConnorM1998 <1612706@rgu.ac.uk>
2018-04-17 23:00:23 +0100

    work

--- lab14/server.js
+++ lab14/server.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var app = express();
+var Twitter = require('twitter')
+
+var client = new Twitter({
+  consumer_key: iAahbawQVn5wQEUju37llcP0j
+  consumer_secret: Li6FwwyOwIPGkmSw0WRPANTYzBhmNcUpll0wX5gdEqjBmhCfTl
+  access_token_key: 986353016549920769-M6Iqx6VHeGLHlhRVOYaocEyj6Trgu5o
+  access_token_secret: Gyae9gkA8vM6kDw8eXt21YOi97Sdb9f2IbRACE6gsUblM
+})
+
+
+//
 app.use(express.static('public'))
 
 app.get('/', function(req, res){
+  //
+  var params = {screen_name: 'nodejs'};
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if(!error){
+      console.log(tweets);
+    }
+  });
+
   res.send("Hello World! by express");
 });
 
