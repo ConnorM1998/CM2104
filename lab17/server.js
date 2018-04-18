dd8499ec32e063f91ed8be71c30f9560bac8955c
ConnorM1998 <1612706@rgu.ac.uk>
2018-04-18 23:17:59 +0100

    w

--- lab17/server.js
+++ lab17/server.js
@@ -9,8 +9,21 @@ app.set('view engine', 'ejs');
 //use res.render to load up an ejs view file
 
 //index pages
+// app.get('/', function(req, res){
+//   res.render('pages/index');
+// });
 app.get('/', function(req, res){
-  res.render('pages/index');
+  var drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 },
+  ];
+  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render('pages/index', {
+    drinks: drinks,
+    tagline: tagline
+  });
 });
 
 //about pages
